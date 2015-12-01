33ccb9e2581c2fe9da69eb5668f99454ee252382
quentin DIAZ <diaz@etud.rcisima.isima.fr>
2015-12-01 09:27:29 +0100

    Controller::loadTwits

--- src/tp9/Tp9.java
+++ src/tp9/Tp9.java
@@ -1,11 +1,9 @@
 package tp9;
 
 import java.awt.BorderLayout;
-import static java.awt.BorderLayout.NORTH;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,26 +15,11 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import tp9.model.TwitterModel;
 import tp9.swingView.SwingTwitterView;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Status;
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
 public class Tp9 {
 
 	public static void main(String[] args) throws TwitterException {
-		
-		Twitter twitter = new TwitterFactory("hP49CWN0KuutGM6PCn7pfVk7P", "vQXz8BZ1tKBiSx1YTRwlkzT0Tfqf72hTdTSHv59hw5T1WFc0V8").createTwitter();
-
-		Query query = new Query("xisumavoid");
-		
-		QueryResult result = twitter.search(query);
-		for (Status s : result.getTweets())
-			System.out.println(s.getText());
-		
-		
-		
 		JFrame frame = new JFrame();
 		JPanel pane = new JPanel(new BorderLayout());
 		JPanel pane2 = new JPanel(new FlowLayout());
@@ -75,6 +58,11 @@ public class Tp9 {
 		final ITwitterView view = new SwingTwitterView();
 		final TwitterModel model = new TwitterModel();
 		final TwitterController ctrl = new TwitterController(model, view);
+		
+		ArrayList<String> user = new ArrayList<>();
+		user.add("xisumavoid");
+		
+		ctrl.loadTwits(user, new ArrayList<String>(), new ArrayList<String>());
 		view.run();
 	}
 }
--- src/tp9/TwitterController.java
+++ src/tp9/TwitterController.java
@@ -1,23 +1,48 @@
 package tp9;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import tp9.model.TwitterModel;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
 public class TwitterController implements Observer {
 	private final TwitterModel _model;
 	private final ITwitterView _view;
+	private final Twitter _twitter;
 	
-	public TwitterController(TwitterModel model, ITwitterView view) {
+	public TwitterController(TwitterModel model, ITwitterView view) throws TwitterException {
 		_model = model;
 		_view = view;
 		
 		_model.addObserver(this);
 		_view.setController(this);
+		
+		_twitter = new TwitterFactory("hP49CWN0KuutGM6PCn7pfVk7P", "vQXz8BZ1tKBiSx1YTRwlkzT0Tfqf72hTdTSHv59hw5T1WFc0V8").createTwitter();
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		
+	}
+	
+	public void loadTwits(List<String> users, List<String> hashtags, List<String> keywords) throws TwitterException {
+		StringBuilder sb = new StringBuilder();
+		for(String user : users)
+			sb.append('@').append(user);
+		for(String hashtag : hashtags)
+			sb.append('#').append(hashtag);
+		for(String keyword : keywords)
+			sb.append(keyword);
+		
+		Query query = new Query(sb.toString());
+		
+		QueryResult result = _twitter.search(query);
+		for (Status s : result.getTweets())
+			System.out.println(s.getText());
 	}
 }
